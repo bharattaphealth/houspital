a76a4458b2405b97532dca375182dedab318fe0d
Bharat <bharat@Bharats-MacBook-Pro-2.local>
2024-09-06 10:22:36 +0530

    stable homescreen and chat screen

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -12,13 +12,13 @@ const CodeSnippet = ({ id }: Props) => {
   const { toast } = useToast()
   let snippet = `
     const iframe = document.createElement("iframe");
-    
+
     const iframeStyles = (styleString) => {
     const style = document.createElement('style');
     style.textContent = styleString;
     document.head.append(style);
     }
-    
+
     iframeStyles('
         .chat-frame {
             position: fixed;
@@ -27,11 +27,11 @@ const CodeSnippet = ({ id }: Props) => {
             border: none;
         }
     ')
-    
+
     iframe.src = "http://localhost:3000/chatbot"
     iframe.classList.add('chat-frame')
     document.body.appendChild(iframe)
-    
+
     window.addEventListener("message", (e) => {
         if(e.origin !== "http://localhost:3000") return null
         let dimensions = JSON.parse(e.data)
